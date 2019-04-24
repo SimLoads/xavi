0fa7c390f437fee6ef1475a2a28323ca867a6ba7
pygoose <35179439+SimLoads@users.noreply.github.com>
2019-04-24 19:58:17 +0100

    Update Xavi.py

--- Xavi.py
+++ Xavi.py
@@ -1,28 +1,30 @@
-print("Modified using Mango 0218190101")
-import sys
-sys.path.insert(0, 'filetype')
 '''
 
 Xavi Standard Audio Service
 Main Tools
 
 Author:: Sam F // PyGoose // https://github.com/SimLoads
-Version:: 041319.1x0001
+Version:: 042419.1x0002
 
 /NOTES/
 
 
 
 '''
-import filetype
+import sys
+sys.path.insert(0, 'xavi_filetype_mod')
+try:
+    import filetype
+except:
+    print("Import fail!")
+    exit()
 def audtest(filename):
     try:
         fType = ((filetype.guess(filename)).mime)
     except FileNotFoundError:
         print("File does not exist!")
         exit()
-    print(fType)
     if "audio" in fType:
-        print("This is a supported format.")
+        print(filename + " is a supported format.")
     else:
         print("Unsupported format.")
