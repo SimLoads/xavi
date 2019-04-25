ed5ff01d90a552e2fd65cd5c9e85ac439e4cee91
pygoose <35179439+SimLoads@users.noreply.github.com>
2019-04-25 10:41:59 +0100

    Refined error catching
    + Built in function for unsupported format catching

--- Xavi.py
+++ Xavi.py
@@ -11,20 +11,24 @@ Version:: 042419.1x0002
 
 
 '''
-import sys
-sys.path.insert(0, 'xavi_filetype_mod')
-try:
-    import filetype
-except:
-    print("Import fail!")
-    exit()
 def audtest(filename):
+    import sys
+    sys.path.insert(0, 'xavi_filetype_mod')
+    try:
+        import filetype
+    except:
+        print("Import fail!")
+        exit()
     try:
         fType = ((filetype.guess(filename)).mime)
     except FileNotFoundError:
         print("File does not exist!")
         exit()
+    except AttributeError:
+        print(filename + " is not supported. ")
+        return()
     if "audio" in fType:
         print(filename + " is a supported format.")
+        return()
     else:
         print("Unsupported format.")
