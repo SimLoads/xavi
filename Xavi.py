d961f914a280ac6066b4a4e7e10e018612c111ff
pygoose <35179439+SimLoads@users.noreply.github.com>
2019-05-02 15:08:18 +0100

    Wav playback
    + Added playback of wave files
    + Modified tone generation slightly

--- Xavi.py
+++ Xavi.py
@@ -38,17 +38,68 @@ def audtest(filename):
     else:
         print("Unsupported format.")
 
-def testwave(x):
+def testwave(length, freq, fname):
     import os
     import numpy as np
     import wave
     import struct
     os.chdir('matplotlib')
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
     # frequency is the number of times a wave repeats a second
-    frequency = 1000
-    num_samples = 48000
+    os.chdir('..')
+    # Frequency
+    try:
+        fr = int(freq)
+    except:
+        print("Invalid argument: " + freq)
+        exit()
+    # Number of samples
+    try:
+        nsam = (int(length) * int(48000))
+    except:
+        print("Invalid argument: " + length)
+        exit()
     # The sampling rate of the analog to digital convert
-    sampling_rate = 48000.0
-    amplitude = 16000
-    file = "test.wav"
+    srate = 48000.0
+    # Amplitude
+    amp = 18000
+    file = (fname + ".wav")
+    nframes=nsam
+    comptype="NONE"
+    compname="not compressed"
+    nchannels=1
+    sampwidth=2
+    sine_wave = [np.sin(2 * np.pi * fr * x/srate) for x in range(nsam)]
+    wav_file=wave.open(file, 'w')
+    wav_file.setparams((nchannels, sampwidth, int(srate), nframes, comptype, compname))
+    for s in sine_wave:
+        wav_file.writeframes(struct.pack('h', int(s*amp)))
+        
+def readwav(filename):
+    import os
+    import sys
+    sys.path.insert(0, os.getcwd())
+    import numpy as np
+    import sounddevice as sd
+    import time
+    from scipy.io import wavfile
+    os.chdir('matplotlib')
+    import matplotlib.pyplot as plt
+    os.chdir('..')
+    try:
+        file = wavfile.read(filename)
+    except ValueError:
+        print("Error with wav file.")
+        print("Is it a converted mp3?")
+        exit()
+    except FileNotFoundError:
+        print("File not found!")
+        exit()
+    waveArray = np.array(file[1],dtype=np.int16)
+    lengthArray = (len(waveArray))
+    hiF = (np.amax(waveArray))
+    loF = (np.amin(waveArray))
+    sd.play(waveArray, 44100)
+    input()
+    sd.stop()
+
