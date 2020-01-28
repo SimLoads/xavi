f76661748421fa5cd02846dadc453cdb93028f35
pygoose <35179439+SimLoads@users.noreply.github.com>
2020-01-28 10:03:58 +0000

    Refined XaviShell
    + Added support for all new commands
    + Added easier futureproofing
    - Removed functions for specifying args
    ~ Changed pretty much everything about how this scirpt functions

--- XaviShell.py
+++ XaviShell.py
@@ -5,9 +5,9 @@ Shell Service
 
 Author:: Sam F // PyGoose // https://github.com/SimLoads
 '''
-Version = '112219.5x0006'
+Version = '012820.5x0008'
 '''
-Release Version:: 0.0.1
+Release Version:: 0.0.2
 
 /NOTES/
 
@@ -34,48 +34,12 @@ def errorHandle(error):
     print(error)
     return()
 
-def xs_cmFormulate(npa,lv,pr):
-    Args = {
-        'dtype':'-d ',
-        'ind':'-i ',
-        'device1':'-fd ',
-        'device2':'-sd ',
-        'freq':'-r ',
-        'length':'-l '
-    }
-    if 'device1' in lv:
-        # Preparing for livebridge
-        print("")
-        command = []
-        command.append(str(confItems[0] +' XaviSNS.py -c livebridge '))
-        fname, ind, dty, dv1, dv2 = npa[0], npa[1], npa[2], npa[3], npa[4]
-        if len(fname) > 0: command.append(str('-f ' + fname + " "))
-        if not ind == '':
-            command.append(str((Args["ind"]) + ind + " "))
-        if not dty == '':
-            command.append(str((Args["dtype"]) + dty + " "))
-        if not dv1 == '':
-            command.append(str((Args["device1"]) + dv1 + " "))
-        if not dv2 == '':
-            command.append(str((Args["device2"]) + dv2 + " "))
-        os.system(''.join(command))
-    elif 'freq' in lv:
-        # Preparing for testwave
-        print("")
-        frq, lng, fname = npa[0], npa[1], npa[2]
-        command = confItems[0] + ' XaviSNS.py -c testwave '
-        command = (command + '-f ' + fname + " ")
-        if not lng == '':
-            command = (command + (Args["length"]) + lng + " ")
-        if not frq == '':
-            command = (command + (Args["freq"]) + frq + " ")   
-        os.system(command)
-    return
 def query(command):
     try:
         print(helpdict.get(command))
     except:
         print("Unrecognized command")
+        
 def startupCheck():
     print("Xavi Shell Service")
     print("Version %s" % Version)
@@ -84,10 +48,9 @@ def startupCheck():
     if os.path.exists("xavi"):
         sys.path.insert(0, 'xavi')
         import Xavi,XaviSNS
-        cmList["livebridge"] =  Xavi.livebridge
-        cmList["testwave"] =  Xavi.testwave
     else:
         errorHandle("Xavi Directory is missing.")
+        input()
         exit()
     print(asLogo)
     takeInput()
@@ -95,11 +58,18 @@ def startupCheck():
 def xs_help():
     print("XaviShell Help\n\nCommands:\n")
     for key, value in cmList.items():
-        print(key)
-def execdir():
+        print('%s: %s' %(key, value))
+
+def execdir(request=False):
+    if request:
+        with open('.xaviconf', 'r') as xfc:
+            current = xfc.read()
+            xfc.close()   
+        return(current.split(' >< ')[0])
+
     with open('.xaviconf', 'r') as xfc:
         current = xfc.read()
-        print("Current command: " + (current.split(' >< ')[0]))
+        print("Current directory: " + (current.split(' >< ')[0]))
         xfc.close()
     with open('.xaviconf', 'w') as xfc:
         ndir = input("Enter absolute directory of Python executable or 'python' to reset: ")
@@ -112,73 +82,45 @@ def execdir():
         else:
             xfc.write(current)
             print("Invalid")
+
 def cmod():
     os.startfile('.xaviconf')
-def force():
-    import Xavi,XaviSNS
-    direct = input("direct|>")
-    if direct == "return":
-        return
-    run = ("python XaviSNS.py -c " + direct)
-    os.system(run)
-    force()
 
-def arg(out, dcLookup, process):
-    import Xavi
-    nPosArgs = []
-    sigvars = (str(inspect.signature(dcLookup)).replace("(","").replace(")","")).replace(' ','').split(',')
-    lVars = len(sigvars)
-    print("Arguments Required: %s" % lVars)
-    for n,l in enumerate(sigvars):
-        tVar = input("%s >> " %l)
-        nPosArgs.append(tVar)
-    if out == 'force_return':
-        return nPosArgs, sigvars, process
-    else:
-        getattr(sys.modules[__name__], out)(nPosArgs, sigvars, process)
+def execCm(process):
+    run = ("%s XaviSNS.py -c %s" %((execdir(True)), process))
+    os.system(run)
+    return
+
+def forceExec(process):
+    run = ("%s XaviSNS.py %s" %((execdir(True)), process))
+    os.system(run)
+    return
 
 def takeInput():
     try:
         print("")
         process = input("xavi|>")
-        # If command is recognized
-        if process in cmList:
-            dcLookup = (cmList[process])
-            # If command is for Xavi
-            if process in xs_reqPosArgs:
-                xaviArgs(dcLookup, process)
-                takeInput()
-            elif "query" in process:
-                com = input("command >> ")
-                query(com)
-                takeInput()
-            # Else 
-            else:
-                if process == "exit":
-                    exit()
-                (dcLookup())
-                takeInput()
-        # Unknown command
+        if "query" in process:
+            com = input("command >> ")
+            query(com)
+            takeInput()
+        elif process in cmList:
+            (cmList[process])()
         else:
-            errorHandle(("Unrecognized Input: %s" % process))
+            if process == "exit":
+                exit()
+            if process[:2] == '--':
+                forceExec(process[1:])
+            execCm(process)
             takeInput()
     except EOFError:
         exit()
     except KeyboardInterrupt:
         exit()
-def xaviArgs(dcLookup, process):
-    if process == "livebridge":
-        if input("method >> ") == "device":
-            os.system(confItems[0] +" XaviSNS.py -c livebridge")
-            return()
-        else:
-            out = 'xs_cmFormulate'
-            arg(out, dcLookup, process)
-            return
+
 cmList = {
     "exit": exit,
     "reboot": reboot,
-    "force": force,
     "edir": execdir,
     "help": xs_help,
     "query": query
@@ -192,12 +134,11 @@ helpdict = {
     "query": "Get help for specific command",
     "help": "Displays the help menu.",
     "livebridge": "See XaviSNS for help.",
-    "testwave": "See XaviSNS for help."
+    "testwave": "See XaviSNS for help.",
+    "tempo": "See XaviSNS for help.",
+    "getsamples": "See XaviSNS for help."
 }
-xs_reqPosArgs = [
-    'livebridge',
-    'testwave'
-]
+
 if __name__ == "__main__":
     defaultItems = "python >< true >< true"
     if os.path.exists(".xaviconf"):
